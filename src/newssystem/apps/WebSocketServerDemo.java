86fe113191e84fe867555454fc9a91622a5b32c1
Sanker <Jan-Eric.Sanker@itelligence.de>
2020-02-27 14:54:09 +0100

    Auskommentieren unrelevanter Sachen

--- src/newssystem/apps/WebSocketServerDemo.java
+++ src/newssystem/apps/WebSocketServerDemo.java
@@ -40,9 +40,8 @@ public class WebSocketServerDemo {
         registerPort.register(receiverB, "Sport");
         registerPort.register(receiverB, "Politik");
 
-        //newsSystem.newNews("Nachricht: Der FC ... hat gewonnen", "Sport");
-        addNewsPort.newNews(new News("Nachricht: Der FC ... hat gewonnen", "Sport"));
-        addNewsPort.newNews(new News("Nachricht: XYZ ist neuer Bundeskanzler", "Politik"));
+        //addNewsPort.newNews(new News("Nachricht: Der FC ... hat gewonnen", "Sport"));
+        //addNewsPort.newNews(new News("Nachricht: XYZ ist neuer Bundeskanzler", "Politik"));
 
 //        Chef chef = new Chef(newsSystem);
 //        chef.manageNews();
@@ -50,9 +49,9 @@ public class WebSocketServerDemo {
         Thread thread = new Thread(chefMockup);
         thread.start();
 
-        registerPort.unregister(receiverB, "Sport");
-        addNewsPort.newNews(new News("Nachricht: Der FC ... hat gewonnen", "Sport"));
-        addNewsPort.newNews(new News("Nachricht: XYZ ist neuer Bundeskanzler", "Politik"));
+        //registerPort.unregister(receiverB, "Sport");
+        //addNewsPort.newNews(new News("Nachricht: Der FC ... hat gewonnen", "Sport"));
+        //addNewsPort.newNews(new News("Nachricht: XYZ ist neuer Bundeskanzler", "Politik"));
 
 //        chef.manageNews();
 
